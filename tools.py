1ad78469bf64115acc12362494e6f3f3424a7d6b
dany550 <dany.valek@seznam.cz>
2025-08-07 18:55:21 +0200

    zatáčecí protokol integrován do jízdy rovně do speed_calculatoru

--- tools.py
+++ tools.py
@@ -260,12 +260,16 @@ class DBase:
 
         return new_speed
     
-    def speed_calculator(self, motor_angle, speed, terminal_speed, g_cons, corector_cons):
+    def speed_calculator(self, motor_angle, speed, terminal_speed, g_cons, corector_cons, turn_speed = 100, turn_brakepoint = 5):
         new_speed = self.accelerator(self.Lw.angle() - self.start_motor_angle, motor_angle, speed, initial_speed=self.average_motor_speed, terminal_speed=terminal_speed)
-        gyro_corection = self.active_areas[1].angle * g_cons
-        shift_corection = self.active_areas[1].y * corector_cons
-        self.L_speed = new_speed + gyro_corection + shift_corection
-        self.R_speed = new_speed - gyro_corection - shift_corection  
+        if abs(self.active_areas[1].angle) <turn_brakepoint:
+            gyro_corection = self.active_areas[1].angle * g_cons
+            shift_corection = self.active_areas[1].y * corector_cons
+            self.L_speed = new_speed + gyro_corection + shift_corection
+            self.R_speed = new_speed - gyro_corection - shift_corection
+        else:
+            self.L_speed = turn_speed * sign(self.active_areas[1].angle)
+            self.R_speed = -turn_speed * sign(self.active_areas[1].angle)
         #print(new_speed, ";", self.L_speed, ";", self.R_speed)
 
     def straight_position(self, x: float, y: float, direction: int = 1, terminal_speed: Number = 50, speed = 500, Area_N: int = 0):
@@ -329,6 +333,64 @@ class DBase:
                 print("task done", self.active_areas[1].x, distance)
                 break
     
+    def straight_g(distance, angle, terminal_speed: Number = 50, speed = 500, Area_N: int = 0):
+        """
+        extra precise straight movement
+        
+        Parameters:
+            - terminal_speed: Number - in deg/s
+            - skippable: bool
+            - speed: Number - in deg/s
+            - taks: object - this is a looppart function that you would like to incorporate into this function (eg. print() -> print ; and it'll start printing empti lines every turn)
+        
+        uses: accelerator, motor_controler, motor_driver, clamp, absclamp
+        """
+
+        #setup
+        g_cons = 20 # gyrocorector constant (its not recomended to set below 2 and above 50)
+        corector_cons = 10 * direction
+        terminal_speed = clamp(terminal_speed, 1000, 50)
+        stop = terminal_speed == 50
+        start_angle = self.active_areas[Area_N].angle
+        direction = clamp(direction, 1, -1)
+        x_shift = (x - self.active_areas[Area_N].x)*direction
+        y_shift = (y - self.active_areas[Area_N].y)
+        self.start_motor_angle = self.Lw.angle()
+        print("navigation running")
+        
+        #print(self.x, self.y, x_shift, y_shift)
+        
+        #trajectory calculator
+        track_angle = angle_mod(degrees(atan2(y_shift, x_shift)))
+        #if direction == 0:
+        #    direction = sign(90 - abs(angle_mod(track_angle - start_angle)), zero=False)
+        track_angle = track_angle * direction
+        distance = sqrt(x_shift**2 + y_shift**2)*direction
+        if distance == 0:
+            print("start=cíl")
+            return None 
+        motor_angle = (distance*360/ (2*pi*self.wheel_radius))
+        self.active_areas[1].dir_reset(track_angle)
+        self.average_motor_speed = (self.Lw.speed() + self.Rw.speed()) / 2
+        
+        self.locate()
+        print("to travel: ", x, y, " distance: ", distance, " ang: ", track_angle)
+        print("i_ang: ", self.active_areas[1].i_angle, "  angle1: ", self.active_areas[1].angle, "angle0: ", self.active_areas[0].angle)
+
+        while True:
+            self.locate()
+            #print("actual position: ", self.active_areas[0].x, self.active_areas[0].y, "  angle: ", self.active_areas[0].angle)
+            self.speed_calculator(motor_angle, speed, terminal_speed, g_cons, corector_cons)
+            #print("L_speed: ", self.L_speed, "  R_speed: ", self.R_speed, " X: ", self.active_areas[1].x, "  Y: ", self.active_areas[1].y, "  angle: ", self.active_areas[1].angle)
+            self.motor_driver(self.L_speed, self.R_speed)
+
+            #motor breaker
+            if abs(self.active_areas[1].x) > abs(distance): 
+                self.motor_braker(stop)
+                #print(stop, self.x, self.y)
+                print("task done", self.active_areas[1].x, distance)
+                break
+
 
     def forward(self, distance, area_N: int = 0, speed = 500):
         # x1, y1 je kde jsem; x2, y2 je kam chci jít
